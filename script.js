aac4f3fd7b026b52831e9e66ddc2b0ae2643959a
Besim Mustafi <besimnaim@gmail.com>
2024-02-05 14:20:24 +0100

    Add player switch function

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var currentPlayer = "circle";
+var currentPlayer = "Besim Mustafi";
+var spielerDiv = document.getElementById("player");
 var spielfeld = [
   ["", "", ""],
   ["", "", ""],
@@ -6,6 +7,8 @@ var spielfeld = [
 ];
 
 function renderSpielfeld() {
+  spielerDiv.innerHTML = "Spieler " + currentPlayer + " ist am Zug";
+
   var contentDiv = document.getElementById("content");
   var tableHTML = "<table>";
 
@@ -13,13 +16,20 @@ function renderSpielfeld() {
     tableHTML += "<tr>";
 
     for (var j = 0; j < 3; j++) {
-      tableHTML += '<td onclick="cellClick(' + i + "," + j + ')">';
-      if (spielfeld[i][j] === "circle") {
+      tableHTML +=
+        '<td onclick="cellClick(' +
+        i +
+        "," +
+        j +
+        ')" class="' +
+        spielfeld[i][j] +
+        '">';
+      if (spielfeld[i][j] === "Besim Mustafi") {
         tableHTML +=
-          '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><circle cx="50" cy="50" r="40" stroke="#fff" stroke-width="6" fill="none"/></svg>';
-      } else if (spielfeld[i][j] === "cross") {
+          '<svg class="circle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="transparent"/></svg>';
+      } else if (spielfeld[i][j] === "Elon Musk") {
         tableHTML +=
-          '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><line x1="20" y1="20" x2="80" y2="80" stroke="#fff" stroke-width="6"/><line x1="80" y1="20" x2="20" y2="80" stroke="#fff" stroke-width="6"/></svg>';
+          '<svg class="cross" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><line x1="20" y1="20" x2="80" y2="80" stroke="black" stroke-width="3"/><line x1="80" y1="20" x2="20" y2="80" stroke="black" stroke-width="3"/></svg>';
       }
       tableHTML += "</td>";
     }
@@ -33,14 +43,37 @@ function renderSpielfeld() {
 
 function cellClick(row, col) {
   if (spielfeld[row][col] === "") {
-    // Überprüfen, ob das Feld leer ist
     spielfeld[row][col] = currentPlayer;
-
+    currentPlayer =
+      currentPlayer === "Besim Mustafi" ? "Elon Musk" : "Besim Mustafi";
     renderSpielfeld();
-
-    // Wechsle den Spieler
-    currentPlayer = currentPlayer === "circle" ? "cross" : "circle";
+    checkWinner();
   }
+}
+
+function checkWinner() {
+  // ... (unchanged)
+}
+
+function announceWinner(winner) {
+  if (winner !== "Unentschieden") {
+    setTimeout(function () {
+      alert("Spieler " + winner + " hat gewonnen!");
+      spielerDiv.innerHTML = "Spieler " + winner + " hat gewonnen!";
+    }, 300);
+  } else {
+    setTimeout(function () {
+      alert("Das Spiel endet unentschieden!");
+      spielerDiv.innerHTML = "Das Spiel endet unentschieden!";
+    }, 300);
+  }
+
+  currentPlayer = "Besim Mustafi";
+  spielfeld = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
 }
 
 // Aufruf der Renderfunktion
